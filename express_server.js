de437b38aab57ea0dfbb102ab2c09a51ee1df9f1
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-01-20 15:15:17 -0500

    removed rendered index/about, accidentally added from tutorial

--- express_server.js
+++ express_server.js
@@ -5,18 +5,6 @@ const PORT = 8080; // default port 8080
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-//use res.render to load up an ejs view file
-
-// index page
-app.get('/', (req, res) => {
-  res.render('pages/index');
-});
-
-// about page
-app.get('/about', (req, res) => {
-  res.render('pages/about');
-});
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
