04e2f1a928e86f92703879c880eb991dbc9158f3
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-01-20 14:41:46 -0500

    set up routes for index & about pages

--- express_server.js
+++ express_server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//use res.render to load up an ejs view file
+
+// index page
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
